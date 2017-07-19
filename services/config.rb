4dcaeda9ef08d78606d527bf7e409687ea1ec996
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-07-19 08:46:25 +0600

    updated jsrunner to display resource id

--- services/config.rb
+++ services/config.rb
@@ -211,7 +211,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta51"
+                   :version => "1.10.7-beta53"
                },
                {
                    :name => "js-yaml",
